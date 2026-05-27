da4ddd1d5373115cae6581f13f4291cceaf065d4
nabilachls <nabilaalyachalisa@gmail.com>
2026-05-27 21:53:23 +0700

    Initialize calculator project

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int pilihan;
+
+    cout << "===== SMART CALCULATOR =====" << endl;
+    cout << "1. Penjumlahan" << endl;
+    cout << "2. Pengurangan" << endl;
+    cout << "3. Keluar" << endl;
+
+    cout << "Pilih menu : ";
+    cin >> pilihan;
+
+    return 0;
+}
