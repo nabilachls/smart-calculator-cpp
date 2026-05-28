ad1cc32f4e16fe633e748d5d944c7d6620ae6d00
nabilachls <nabilaalyachalisa@gmail.com>
2026-05-28 23:23:46 +0700

    Add scientific calculator features

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 double tambah(double a, double b) {
@@ -17,6 +19,14 @@ double bagi(double a, double b) {
     return a / b;
 }
 
+double pangkat(double a, double b) {
+    return pow(a, b);
+}
+
+double akar(double a) {
+    return sqrt(a);
+}
+
 int main() {
 
     int pilihan;
@@ -27,32 +37,65 @@ int main() {
     cout << "2. Pengurangan" << endl;
     cout << "3. Perkalian" << endl;
     cout << "4. Pembagian" << endl;
+    cout << "5. Pangkat" << endl;
+    cout << "6. Akar Kuadrat" << endl;
 
     cout << "Pilih menu : ";
     cin >> pilihan;
 
-    cout << "Masukkan angka pertama : ";
-    cin >> a;
-
-    cout << "Masukkan angka kedua : ";
-    cin >> b;
-
     switch(pilihan) {
 
         case 1:
+            cout << "Masukkan angka pertama : ";
+            cin >> a;
+            cout << "Masukkan angka kedua : ";
+            cin >> b;
+
             hasil = tambah(a, b);
             break;
 
         case 2:
+            cout << "Masukkan angka pertama : ";
+            cin >> a;
+            cout << "Masukkan angka kedua : ";
+            cin >> b;
+
             hasil = kurang(a, b);
             break;
 
         case 3:
+            cout << "Masukkan angka pertama : ";
+            cin >> a;
+            cout << "Masukkan angka kedua : ";
+            cin >> b;
+
             hasil = kali(a, b);
             break;
 
         case 4:
+            cout << "Masukkan angka pertama : ";
+            cin >> a;
+            cout << "Masukkan angka kedua : ";
+            cin >> b;
+
             hasil = bagi(a, b);
+            break;
+
+        case 5:
+            cout << "Masukkan angka : ";
+            cin >> a;
+
+            cout << "Masukkan pangkat : ";
+            cin >> b;
+
+            hasil = pangkat(a, b);
+            break;
+
+        case 6:
+            cout << "Masukkan angka : ";
+            cin >> a;
+
+            hasil = akar(a);
             break;
 
         default:
